d6cbeb51af26b29aac3b0773ea039f7dbd596cf6
benny.qi <15596637957@163.com>
2022-10-04 02:04:22 -0700

    [Title]Add overturn function
    [Solution]
    add overturn function
    
    [TestReport]
    test pass

---
+++ array/array.h
@@ -0,0 +1,9 @@
+#ifndef __ARRAY_H_
+#define __ARRAY_H_
+
+#define N  10
+int *CreateArray(void);
+void FreeArray(int *pr);
+void ShowArray(int *pr, int lenth);
+int OverturnArray(int *pr, int lenth);
+#endif
--- array/test.c
+++ array/test.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "array.h"
+
 
 int main(void)
 {
+	int *p = NULL;
+
 	printf("test start: \n");
+	p = CreateArray();
+	if (!p) {
+		printf("CreateArray faild \n");
+		return -1;
+	}
 
-
+	ShowArray(p, N);
+	OverturnArray(p, N);
+	ShowArray(p, N);
 
 	printf("test end \n");
+
 	return 0;
 }
